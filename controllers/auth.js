c33568491c45567c04b6650885d2ac43e0fde3f8
nickty <nickty.86@gmail.com>
2021-07-03 14:34:24 +0600

    user info save in back db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,18 @@
-exports.createOrUpdateUser = (req, res) => {
-    res.send('hitss')
+const User = require('../models/user')
+
+exports.createOrUpdateUser = async (req, res) => {
+    
+    const { name, picture, email } = req.user
+
+    const user = await User.findOneAndUpdate({email}, {name, picture}, {new: true})
+
+    if(user){
+        res.json(user)
+    } else {
+        const newUser = await new User({
+            email, name, picture
+        }).save()
+
+        res.json(newUser)
+    }
 }
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,21 @@
 const admin = require('../firebase')
 
-exports.authCheck = (req, res, next) => {
-    console.log(req.headers)
-    next()
+exports.authCheck = async (req, res, next) => {
+    // console.log(req.headers)
+
+    try {
+
+        const firebaseUser = await admin.auth().verifyIdToken(req.headers.authtoken)
+
+        console.log(firebaseUser)
+
+        req.user = firebaseUser
+
+        next()
+        
+    } catch (error) {
+        res.status(401).json({
+            err: 'Invalid or expired token'
+        })
+    }
 }
