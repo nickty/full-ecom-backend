0316ee7662507b2001987df265e9d1061594d429
nickty <nickty.86@gmail.com>
2021-07-03 15:01:44 +0600

    save name also into db

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,13 +4,13 @@ exports.createOrUpdateUser = async (req, res) => {
     
     const { name, picture, email } = req.user
 
-    const user = await User.findOneAndUpdate({email}, {name, picture}, {new: true})
+    const user = await User.findOneAndUpdate({email}, {name: email.split('@')[0], picture}, {new: true})
 
     if(user){
         res.json(user)
     } else {
         const newUser = await new User({
-            email, name, picture
+            email, name: email.split('@')[0], picture
         }).save()
 
         res.json(newUser)
