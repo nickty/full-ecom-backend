b62a2ba330bcc84aaa97a12548989d9cbea154f0
nickty <nickty.86@gmail.com>
2021-07-03 12:55:30 +0600

    fire base primarity initialized with backend

---
+++ firebase/index.js
@@ -0,0 +1,8 @@
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require('../config/fbServerAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
