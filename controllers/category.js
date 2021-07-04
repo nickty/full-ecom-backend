63d050fa07a1a6ed06b94f53e95ff017b22d983f
nickty <nickty.86@gmail.com>
2021-07-04 08:17:27 +0600

    category backend done

--- controllers/category.js
+++ controllers/category.js
@@ -20,16 +20,38 @@ exports.create = async (req, res) => {
 
 exports.list = async (req, res) => {
 
+    res.json( await Category.find({}).sort({createdAT: -1}).exec())
+
 }
 
 exports.read = async (req, res) => {
+
+    let category = await Category.findOne({slug: req.params.slug}).exec();
+
+    res.json(category)
 
 }
 
 exports.update = async (req, res) => {
 
+    const { name } = req.body
+
+    try {
+        const updated = await Category.findOneAndUpdate({slug: req.params.slug}, {name, slug: slugify(name)}, {new: true})
+        res.json(updated)
+    } catch (error) {
+        console.log(error)
+        res.status(400).send('Category update failed')
+    }
+
 }
 
 exports.remove = async (req, res) => {
-
+    try {
+        const deleted = await Category.findOneAndDelete({slug: req.params.slug})
+        res.json(deleted)
+    } catch (error) {
+        console.log(error)
+        res.status(400).send('Delete failed')
+    }
 }
