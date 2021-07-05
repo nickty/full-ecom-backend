f56e0d079d7bd5b89d82cdffea55818267ce25e9
nickty <nickty.86@gmail.com>
2021-07-05 11:32:35 +0600

    little update for shipping

--- models/product.js
+++ models/product.js
@@ -26,25 +26,25 @@ const productSchema = new mongoose.Schema({
         trim: true, 
         required: true, 
     },
-    category: {
-        type: ObjectId,
-        ref: 'Category'
-    },
-    subs: [{
-        type: ObjectId,
-        ref: 'Sub'
-    }],
+    // category: {
+    //     type: ObjectId,
+    //     ref: 'Category'
+    // },
+    // subs: [{
+    //     type: ObjectId,
+    //     ref: 'Sub'
+    // }],
     quantity: Number,
     sold: {
         type: Number,
         default: 0
     },
-    images: {
-        type: Array
-    },
+    // images: {
+    //     type: Array
+    // },
     shipping: {
         type: String,
-        enum: ['Yes', "No"]
+        enum: ['yes', "no"]
     },
     color: {
         type: String,
