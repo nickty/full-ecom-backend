373f42c37c5b86e62d43c4df92f07ba09d8fab56
nickty <nickty.86@gmail.com>
2021-07-17 15:21:37 +0600

    fix

--- routes/coupon.js
+++ routes/coupon.js
@@ -4,7 +4,7 @@ const { authCheck, adminCheck } = require('../middlewares/auth')
 const router = express.Router()
 
 router.post('/coupon', authCheck, adminCheck, create)
-router.get('/coupon', list)
+router.get('/coupons', list)
 router.delete('/coupon/:couponId', authCheck, adminCheck, remove)
 
 
