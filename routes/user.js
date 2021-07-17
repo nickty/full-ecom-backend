cc34568786b1ce973de278846d42e674def3cc39
nickty <nickty.86@gmail.com>
2021-07-17 11:28:45 +0600

    empty the card

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { userCart, getCart } = require('../controllers/user')
+const { userCart, getCart, emptyCart } = require('../controllers/user')
 const { authCheck } = require('../middlewares/auth')
 
 const router = express.Router()
 
 router.post('/user/cart', authCheck, userCart)
 router.get('/user/cart', authCheck, getCart)
+router.get('/user/cart', authCheck, emptyCart)
 
 module.exports = router
