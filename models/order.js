d51d5d29101e46f0925812b856fd27d3bc4795a1
nickty <nickty.86@gmail.com>
2021-07-18 22:10:50 +0600

    order schema

---
+++ models/order.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const { ObjectId } =  mongoose.Schema
+const Schema = mongoose.Schema
+
+
+const orderSchema = new Schema({
+    products:  [
+        {
+            product: {
+                type: ObjectId, 
+                ref: 'Product'
+            }, 
+            count: Number, 
+            color: String, 
+           
+        }, 
+       
+
+    ], 
+    paymentIntent: {},
+    orderStatus: {
+        type: String, 
+        default: 'Not processed', 
+        enum:[
+            "Not processed", 
+            "processing",
+            "dispatched",
+            "cancelled",
+            "completed"
+        ]
+    }, 
+    orderedBy: {
+        type: ObjectId, 
+        ref: "User"
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model("Order", orderSchema)
