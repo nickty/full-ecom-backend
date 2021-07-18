15792d2ee12c2e612c5b6abce588fc43e9472334
nickty <nickty.86@gmail.com>
2021-07-18 07:37:24 +0600

    send actual amount to stripe

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -7,9 +7,15 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET)
 
 exports.createPaymentIntent = async (req, res) => {
     //later applie coupon 
+    
+    const user = await User.findOne({email: req.user.email}).exec()
+
+    const { cartTotal } = await Cart.findOne({orderedBy: user._id})
+
+    console.log('Cart total change', cartTotal)
 
     const paymentIntent = await stripe.paymentIntents.create({
-        amount: 100,
+        amount: cartTotal * 100,
         currency: 'usd'
     })
 
