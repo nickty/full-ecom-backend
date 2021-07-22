58c79e3cdaa75cfcec9a0f556e00d90d39a1550c
nickty <nickty.86@gmail.com>
2021-07-22 08:26:30 +0600

    getting orders for logged in user

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { userCart, getCart, emptyCart, saveAddress, applyCouponToUserCart, createOrder } = require('../controllers/user')
+const { userCart, getCart, emptyCart, saveAddress, applyCouponToUserCart, createOrder, orders } = require('../controllers/user')
 const { authCheck } = require('../middlewares/auth')
 
 const router = express.Router()
@@ -12,5 +12,7 @@ router.post('/user/address', authCheck, saveAddress)
 router.post('/user/cart/coupon', authCheck, applyCouponToUserCart)
 
 router.post('/user/order', authCheck, createOrder)
+
+router.get('/user/orders', authCheck, orders)
 
 module.exports = router
